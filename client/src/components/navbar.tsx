9f65d64ac26baf8f7bbd9d4ee91283b07a90edeb
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-16 14:08:47 -0600

    ♻️ Add FaPaperclip icon to navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
+import { FaPaperclip } from "react-icons/fa6";
 import { ModeToggle } from "./theme-toggle-button";
 import { buttonVariants } from "./ui/button";
 
 function Navbar() {
   return (
     <nav className="flex justify-between">
-      <h1>Shortify</h1>
+      <div className="flex items-center">
+        <FaPaperclip size={20} />
+        <h1 className="ml-2">Shortify</h1>
+      </div>
       <div className="flex gap-x-2 items-center">
         <Link
           className={buttonVariants({ variant: "outline" })}
