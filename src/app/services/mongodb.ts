3d1432f2e1873563c2603bb6d22f4f874ab10abd
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-17 13:41:36 -0600

    🚀 Refactor MongoDB connection code

--- src/app/services/mongodb.ts
+++ src/app/services/mongodb.ts
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL as string);
+    const dbUrl = process.env.DATABASE_URL;
+    if (!dbUrl) {
+      throw new Error("DATABASE_URL is not defined");
+    }
+
+    await mongoose.connect(dbUrl);
 
     mongoose.connection.on("connected", () => {
       console.log("Connected to database");
@@ -10,6 +15,10 @@ export const connectToDatabase = async () => {
 
     mongoose.connection.on("error", (error) => {
       console.error("Error while connecting to database:", error);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.log("Disconnected from database");
     });
   } catch (error) {
     console.error("Error while connecting to database:", error);
