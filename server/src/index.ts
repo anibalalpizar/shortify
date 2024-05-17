31ccd6c7d6e4fccfcf817e2a1054232f671e2f32
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-05-17 00:37:57 -0600

    🗑️ Remove unnecessary server files and configurations

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import { PORT } from "./conf/config";
-import Logger from "./libs/logger";
-
-app.listen(app.get("port"), () => {
-  Logger.info(`Server running on port ${PORT}`);
-});
-
-app.listen();
